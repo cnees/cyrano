3d6920bee6c1d8ff046e7bae93fbcb4eec093c5f
Clara Nees <cnees@umich.edu>
2016-07-05 17:38:21 -0400

    Add form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,7 @@
 class HomeController < ApplicationController
+  def index
+    if(params.has_key?(:syn) && params.has_key?(:rhyme))
+      render :json => {syn: params[:syn], rhyme: params[:rhyme], test: "success"};
+    end
+  end
 end
